b4c81aca925a583d6efb22fae33de26dcb3c3265
cakelemon <cakelemon13@gmail.com>
2019-12-22 20:16:45 +0900

    필요할 것 같은 자료구조 (map, tile, encryptedVar) 뼈대 추가

---
+++ BrainlessPenguins/Assets/Scripts/EncryptedVar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EncryptedVar<T>
+{
+    public T get()
+    {
+        return _elem;
+    }
+
+    public void set(T elem)
+    {
+        _elem = elem;
+    }
+
+    T _elem;
+}
---
+++ BrainlessPenguins/Assets/Scripts/Map.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile
+{
+    enum TileType
+    {
+        grass,
+        ice
+    }
+
+    TileType _type;
+}
+
+public class Map
+{
+    public int getHeight()
+    {
+        return _tiles.Count;
+    }
+
+    public int getWidth()
+    {
+        return _tiles[0] is null ? 0 : _tiles[0].Count;
+    }
+
+    List<List<Tile>> _tiles;
+}

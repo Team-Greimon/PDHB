9fc9f486e2ef14d9f6640e416f85a85ed90805bb
cakelemon <cakelemon13@gmail.com>
2019-12-22 22:42:09 +0900

    펭귄

---
+++ BrainlessPenguins/Assets/Scripts/Penguin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Penguin : MonoBehaviour
+{
+    public enum PenguinType
+    {
+        red = 0,
+        black = 1,
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    PenguinType _penguinType;
+}

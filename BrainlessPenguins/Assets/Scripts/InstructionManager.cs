4787a65934c23ff5017e3d5fec98770fc3777795
cakelemon <cakelemon13@gmail.com>
2019-12-22 23:25:54 +0900

    Instruction 자료구조

---
+++ BrainlessPenguins/Assets/Scripts/InstructionManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instruction
+{
+    public class Condition
+    {
+        public enum ConditionType
+        {
+            tileCollision,
+            penguinCollision,
+            always
+        }
+        public int _param;
+    }
+
+    public class Action
+    {
+        public enum ActionType
+        {
+            turn
+        }
+        public ActionType _actionType;
+    }
+
+    Condition _condition;
+    Action _action;
+}
+
+public class InstructionManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

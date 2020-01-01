3335ec15208dbda047f424d31d644b32bc026345
cakelemon <cakelemon13@gmail.com>
2020-01-01 23:31:14 +0900

    IntegerStream 구현

--- BrainlessPenguins/Assets/Scripts/Util/StringUtils.cs
+++ BrainlessPenguins/Assets/Scripts/Util/StringUtils.cs
@@ -4,6 +4,36 @@ using System.IO;
 using System.Text;
 using UnityEngine;
 
+public class IntegerStream
+{
+    public IntegerStream(string s)
+    {
+        var arr = s.Split(null);
+        foreach (var token in arr)
+        {
+            if (!string.IsNullOrEmpty(token))
+            {
+                _intArray.Add(int.Parse(token));
+            }
+        }
+    }
+    public int Read()
+    {
+        if (_position < _intArray.Count)
+        {
+            return _intArray[_position++];
+        }
+        else
+        {
+            Debug.LogError("IntegerStream 의 범위를 초과하여 읽고 있습니 ");
+            return -1;
+        }
+    }
+
+    readonly List<int> _intArray = new List<int>();
+    int _position;
+}
+
 public static class StringUtils
 {
     public static MemoryStream GenerateStreamFromString(string value)
